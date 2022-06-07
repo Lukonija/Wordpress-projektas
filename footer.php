d95bccff5554eaded8daf9e86b6d65b7d6119e17
domsius <dominykas.luksys@gmail.com>
2022-06-07 13:46:02 +0300

    update

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="index-footer">
+        <div class="container-footer">
+            <p>&copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> Laikas dirbti, UAB
+            </p>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <header class="header index-header">
+        <div class="container">
+            <div class="logo">
+                <a href="#"><img src="/assets/img/Laikas-Dirbti-Logo.1.jpg" alt="logo" /></a>
+            </div>
+            <div class="main-navigation">
+                <div class="buttons">
+                    <button class="apie-mus"><a href="/assets/html/laikasdirbti_1.html">Apie mus</a></button>
+                    <button class="specialistu-paieska">
+                        <a href="/assets/html/laikasdirbti_2.html">Specialistų paieška</a>
+                    </button>
+                    <button class="kontaktai"><a href="/assets/html/laikasdirbti_3.html">Kontaktai</a></button>
+                </div>
+            </div>
+            <button class="hamburger">
+                <i class="menuIcon material-icons">menu</i>
+                <i class="closeIcon material-icons">close</i>
+            </button>
+        </div>
+        </div>
+    </header>
