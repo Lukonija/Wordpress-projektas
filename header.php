72edf94d00ca36a712d592116696846eb5049466
Lukonija <vaicekauskaslukas@gmail.com>
2022-06-08 15:33:57 +0300

    navigacijos taskeliai

--- header.php
+++ header.php
@@ -21,13 +21,25 @@
                 array(
                     'menu' => 'primary',
                     'container' => '',
-                    'theme_location' => 'primary'
-                    'items_wrap' => '<div>%3$s</div>'
+                    'theme_location' => 'primary',
+                    'items_wrap' => '<div class="main-navigation">%3$s</div>'
                 )
             );
             ?>
-            <div class="main-navigation">
+            <!-- <div class="main-navigation">
                 <div class="buttons">
+                    <button class="apie-mus"><a href="/assets/html/laikasdirbti_1.html">Apie mus</a></button>
+                    <button class="specialistu-paieska">
+                        <a href="/assets/html/laikasdirbti_2.html">Specialistų paieška</a>
+                    </button>
+                    <button class="kontaktai"><a href="/assets/html/laikasdirbti_3.html">Kontaktai</a></button>
+                </div> -->
+        </div>
+        <div class="navigation">
+            <input type="checkbox" class="toggle-menu" />
+            <div class="hamburger"></div>
+            <div class="menu">
+                <div class="buttons-toggle">
                     <button class="apie-mus"><a href="/assets/html/laikasdirbti_1.html">Apie mus</a></button>
                     <button class="specialistu-paieska">
                         <a href="/assets/html/laikasdirbti_2.html">Specialistų paieška</a>
@@ -35,17 +47,5 @@
                     <button class="kontaktai"><a href="/assets/html/laikasdirbti_3.html">Kontaktai</a></button>
                 </div>
             </div>
-            <div class="navigation">
-                <input type="checkbox" class="toggle-menu" />
-                <div class="hamburger"></div>
-                <div class="menu">
-                    <div class="buttons-toggle">
-                        <button class="apie-mus"><a href="/assets/html/laikasdirbti_1.html">Apie mus</a></button>
-                        <button class="specialistu-paieska">
-                            <a href="/assets/html/laikasdirbti_2.html">Specialistų paieška</a>
-                        </button>
-                        <button class="kontaktai"><a href="/assets/html/laikasdirbti_3.html">Kontaktai</a></button>
-                    </div>
-                </div>
-            </div>
+        </div>
     </header>
