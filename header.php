02b29a2ab6f7e1a0fd0b42058b6157cc36d5d9ad
domsius <dominykas.luksys@gmail.com>
2022-06-09 14:37:47 +0300

    fixed path

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <?php wp_head(); ?>
 </head>
 
@@ -13,7 +12,7 @@
     <header class="header index-header">
         <div class="container">
             <div class="logo">
-                <a href="#"><img src="/wp-content/themes/projektas/theme-template/img/Laikas-Dirbti-Logo.1.jpg" alt="logo" /></a>
+                <a href="/projektas"><img src="/wp-content/themes/projektas/theme-template/img/Laikas-Dirbti-Logo.1.jpg" alt="logo" /></a>
             </div>
             <?php
             wp_nav_menu(
